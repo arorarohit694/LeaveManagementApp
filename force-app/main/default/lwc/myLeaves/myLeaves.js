52ad560d13be89c9f1c3c2462442df61020a863b
arorarohit694 <arorarohit694@gmail.com>
2023-11-28 03:14:10 +0530

    commit

--- force-app/main/default/lwc/myLeaves/myLeaves.js
+++ force-app/main/default/lwc/myLeaves/myLeaves.js
@@ -2,94 +2,94 @@ import { LightningElement, wire } from 'lwc';
 import getMyLeaves from '@salesforce/apex/LeaveRequstController.getMyLeaves';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import Id from '@salesforce/user/Id';
-import {refreshApex} from '@salesforce/apex';
-const COLUMNS =[
-    {label:'Request Id',fieldName:'Name',cellAttributes:{class:{fieldName:'cellClass'}}},
-    {label:'From Date',fieldName:'From_Date__c',cellAttributes:{class:{fieldName:'cellClass'}}},
-    {label:'To Date',fieldName:'To_Date__c',cellAttributes:{class:{fieldName:'cellClass'}}},
-    {label:'Reason',fieldName:'Reason__c',cellAttributes:{class:{fieldName:'cellClass'}}},
-    {label:'Status',fieldName:'Status__c',cellAttributes:{class:{fieldName:'cellClass'}}},
-    {label:'Manager Comment',fieldName:'Manager_Comment__c',cellAttributes:{class:{fieldName:'cellClass'}}},
+import { refreshApex } from '@salesforce/apex';
 
+const COLUMNS = [
+    { label: 'Request Id', fieldName: 'Name', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'From Date', fieldName: 'From_Date__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'To Date', fieldName: 'To_Date__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Reason', fieldName: 'Reason__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Status', fieldName: 'Status__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Manager Comment', fieldName: 'Manager_Comment__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
     {
-        type:"button",typeAttributes:{
-            label:'Edit',
-            name:'Edit',
-            title:'Edit',
-            value:'edit',
-            disabled:{fieldName:'isEditDisabled'}
-        },cellAttributes:{class:{fieldName:'cellClass'}}
+        type: "button", typeAttributes: {
+            label: 'Edit',
+            name: 'Edit',
+            title: 'Edit',
+            value: 'edit',
+            disabled: { fieldName: 'isEditDisabled' }
+        }, cellAttributes: { class: { fieldName: 'cellClass' } }
     }
 ];
-
 export default class MyLeaves extends LightningElement {
-    columns=COLUMNS;
-    myLeaves=[];
+    columns = COLUMNS;
+
+    myLeaves = [];
     myLeavesWireResult;
-    showModalPopup=false;
-    recordId ='';
-    CurrentUserId =Id;
-    objectApiName='LeaveRequest__c';
+    showModalPopup = false;
+    objectApiName = 'LeaveRequest__c';
+    recordId = '';
+    currentUserId = Id;
     @wire(getMyLeaves)
-    wiredMyLeaves(result){
-        this.MyLeavesWireResult=result;
-        if(result.data){
-            this.MyLeaves=result.data.map(a =>({
+    wiredMyLeaves(result) {
+        this.myLeavesWireResult = result;
+        if (result.data) {
+            this.myLeaves = result.data.map(a => ({
                 ...a,
-                cellClass:a.Status__c == 'Approved'?'slds-theme_success':a.Status__c =='Rejected' ? 'slds-theme_warning':'',
-                isEditDisabled:a.Status__c!='Pending'
+                cellClass: a.Status__c == 'Approved' ? 'slds-theme_success' : a.Status__c == 'Rejected' ? 'slds-theme_warning' : '',
+                isEditDisabled: a.Status__c != 'Pending'
             }));
         }
-        if(result.error){
-            console.log('Error Occured while fetching my leaves-',result.error);
+        if (result.error) {
+            console.log('Error occured while fetching my leaves- ', result.error);
         }
     }
-    onRequestClickHandler(){
-        this.showModalPopup=true;
-        this.recordId='';
+
+    get noRecordsFound() {
+        return this.myLeaves.length == 0;
     }
-    popupCloseHandler(){
-        this.showModalPopup=false;
+
+    newRequestClickHandler() {
+        this.showModalPopup = true;
+        this.recordId = '';
     }
-    rowActionHandler(event){
-        this.showModalPopup=true;
-        this.recordId=event.detail.row.Id;
+    popupCloseHandler() {
+        this.showModalPopup = false;
     }
-    successHandler(event){
-        this.showModalPopup=false;
-        const evt = ShowToastEvent({
-            title :'SUCCESS',
-            message :'Data saved successfully',
-            variant :'success'
-        });
-        this.dispatchEvent(evt);
-        refreshApex(this.MyLeavesWireResult);
+
+    rowActionHandler(event) {
+        this.showModalPopup = true;
+        this.recordId = event.detail.row.Id;
+    }
+
+    successHandler(event) {
+        this.showModalPopup = false;
+        this.showToast('Data saved successfully');
+        refreshApex(this.myLeavesWireResult);
 
         const refreshEvent = new CustomEvent('refreshleaverequests');
         this.dispatchEvent(refreshEvent);
     }
-    OnSubmitHandler(event){
+    submitHandler(event) {
         event.preventDefault();
-        const fields= { ...event.detail.fields };
-        fields.Status__c ='Pending';
-        if(new Date(fields.From_Date__c) > new Date(fields.To_Date__c)){
-            const evt = ShowToastEvent({
-                title :'ERROR',
-                message :'To Date should be greater than from date',
-                variant :'error'
-            });
-            this.dispatchEvent(evt);
+        const fields = { ...event.detail.fields };
+        fields.Status__c = 'Pending';
+        if (new Date(fields.From_Date__c) > new Date(fields.To_Date__c)) {
+            this.showToast('From date should not be grater then to date', 'Error', 'error');
         }
-        else if(new Date()>new Date(fields.From_Date__c)){
-            const evt = ShowToastEvent({
-                title :'ERROR',
-                message :'From date should be greater than today date',
-                variant :'error'
-            });
-            this.dispatchEvent(evt);        }
-        else{
-            this.refs.leaveRequestsFrom.submit(fields);
+        else if (new Date() > new Date(fields.From_Date__c)) {
+            this.showToast('From date should not be less then Today', 'Error', 'error');
+        }
+        else {
+            this.refs.leaveReqeustFrom.submit(fields);
         }
     }
-
+    showToast(message, title = 'success', variant = 'success') {
+        const event = new ShowToastEvent({
+            title,
+            message,
+            variant
+        });
+        this.dispatchEvent(event);
+    }
 }
